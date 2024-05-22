e4fbc3f1da55527e005f86b2601b81ab8b6f48c2
Joyson J <joysonjustinjl@gmail.com>
2024-05-22 12:59:24 +0530

    hi

--- Pyspark/demo.py
+++ Pyspark/demo.py
@@ -1,44 +1,26 @@
-from pyspark.sql import SparkSession
 import json
 from flatten_json import flatten
 
-# Initialize SparkSession
-spark = SparkSession.builder \
-    .appName("ReadLogsToDataFrame") \
-    .getOrCreate()
+# Path to your JSON file
+json_file_path = r"D:\Data_Engineering_VirEnv\dtd_logs\dtdlogs_demo.json"
+# Path to your flattened JSON file
+flattened_json_file_path = "flattened_json_file.json"
 
-# Path to the folder containing log files
-folder_path = r'D:\Data_Engineering_VirEnv\Java_Microservices_Logs\demoauth'
+# Load JSON data from the file
+with open(json_file_path, 'r') as file:
+    json_data = json.load(file)
 
-# List all log files in the folder
-log_files = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if file.endswith('.log')]
+# Check if the loaded data is a list
+if not isinstance(json_data, list):
+    raise ValueError("The JSON data should be a list of JSON objects.")
 
-# Initialize an empty list to store RDDs
-parsed_logs_rdds = []
+# Flatten each JSON object in the list
+flattened_data = [flatten(record) for record in json_data]
 
-# Process each log file
-for log_file in log_files:
-    # Read log file as text file
-    log_data = spark.sparkContext.textFile(log_file)
-    
-    # Parse each line as JSON
-    parsed_logs = log_data.map(lambda line: json.loads(line))
-    
-    print(f"Contents of parsed RDD for file {log_file}:")
-    print(parsed_logs.collect())  # Collect and print all elements
-    # Append parsed RDD to the list
-    parsed_logs_rdds.append(parsed_logs)
+# # Save the flattened data to a new file
+with open(flattened_json_file_path, 'w') as file:
+    json.dump(flattened_data, file, indent=4)
 
-# Union all RDDs
-all_logs_rdd = spark.sparkContext.union(parsed_logs_rdds)
-print("unioned data", all_logs_rdd.collect())
-
-# Create DataFrame from parsed JSON data
-df = spark.createDataFrame(all_logs_rdd)
-
-# Show DataFrame schema and sample data
-df.printSchema()
-df.show()
-
-# Stop SparkSession
-spark.stop()
+# Print the flattened data to console
+for record in flattened_data:
+    print(record)
---
+++ Pyspark/demo1.py
@@ -0,0 +1,94 @@
+import json
+from flatten_json import flatten
+from pyspark.sql import SparkSession
+import pandas as pd
+from pyspark.sql.types import *
+from pyspark.sql.functions import regexp_replace, col
+
+# Initialize a Spark session
+spark = SparkSession.builder \
+    .appName("JSON to CSV with Defined Schema") \
+    .getOrCreate()
+    
+    
+stucture_details={
+  "/hardware-interface": {
+    "schema": "schema1",
+    "table": "dtdlogs"
+  },
+  "page_name2": {
+    "schema": "schema2",
+    "table": "test2"
+  }
+}
+
+
+# Path to your JSON file
+json_file_path = "Admin_24-1-2024- 6-50-52 pm 1 copy.json"
+
+# Load JSON data from the file
+with open(json_file_path, 'r') as file:
+    json_data = json.load(file)
+
+# Check if the loaded data is a list
+if not isinstance(json_data, list):
+    raise ValueError("The JSON data should be a list of JSON objects.")
+
+# Flatten each JSON object in the list
+flattened_data = [flatten(record) for record in json_data]
+
+
+
+# Define the schema for the DataFrame
+# You may need to customize this schema based on your actual data
+schema1 = StructType([
+    StructField("createAt", StringType(), True),
+    StructField("user", StringType(), True),
+    StructField("loginAt", StringType(), True),
+    StructField("logoutAt", StringType(), True),
+    StructField("request_event", StringType(), True),
+    StructField("request_page", StringType(), True),
+    StructField("request_action", StringType(), True),
+    StructField("request_dllCallMethod", StringType(), True),
+    StructField("response_event", StringType(), True),
+    StructField("response_data_user", StringType(), True),
+    StructField("response_data_loginAt", StringType(), True),
+    StructField("response_data_isLogin", BooleanType(), True),
+    StructField("response_data_logoutAt", StringType(), True),
+    StructField("response_data_isLogout", BooleanType(), True),
+    StructField("response_data_status", IntegerType(), True),
+    StructField("response_data_message", StringType(), True),
+    StructField("response_data_error", BooleanType(), True),
+    StructField("response_data_dllCallMethod", StringType(), True),
+    StructField("response_data_vciList", StringType(), True),
+])
+
+# Define JDBC connection properties
+jdbc_url = "jdbc:mysql://127.0.0.1:3306/test"
+connection_properties = {
+    "user": "root",
+    "password": "",
+    "driver": "com.mysql.jdbc.Driver"
+}
+
+# Create DataFrame for each flattened JSON object and print it
+for record in flattened_data:
+    if record.get("request_page")[1:] == "hardware-interface":
+        # Ensure the record matches the schema
+        record_with_schema = {k: record.get(k) for k in schema1.fieldNames()}
+        print("record_with_schema",record_with_schema)
+        df = spark.createDataFrame([record_with_schema], schema=schema1)
+        df = df.withColumn("request_page", regexp_replace(regexp_replace(col("request_page"), "^/", ""), "-", " "))
+        #df.show(truncate=False) 
+        #df.write.jdbc(url=jdbc_url, table="dtdlogs", mode="append", properties=connection_properties)
+
+
+spark.stop()
+
+
+
+
+
+
+
+
